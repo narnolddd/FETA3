d34716db1ac5835c91f32d1c14901fe33c946fa7
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2020-06-05 19:02:22 +0100

    slight efficiency improvement in triangle model

--- feta/objectmodels/TriangleClosure2.java
+++ feta/objectmodels/TriangleClosure2.java
@@ -11,21 +11,18 @@ import java.util.Set;
 public class TriangleClosure2 extends ObjectModelComponent{
 
     private int[] occurrences_;
-    private int [] justChosen_;
     private boolean random_;
 
 
-    @Override
     public void calcNormalisation (UndirectedNetwork net, int[] removed) {
-        int total=0;
-        occurrences_ = new int[net.noNodes_+1];
-        justChosen_=removed;
-        if (justChosen_.length==0) {
+        if (removed.length==0) {
             normalisationConstant_=net.noNodes_;
             random_=true;
         }
         else {
-            int node = justChosen_[justChosen_.length - 1];
+            int total=0;
+            occurrences_ = new int[net.noNodes_+1];
+            int node = removed[removed.length - 1];
             for (int n1 : net.neighbours_.get(node)) {
                 for (int n2 : net.neighbours_.get(n1)) {
                     if (n2 == node)
@@ -49,15 +46,14 @@ public class TriangleClosure2 extends ObjectModelComponent{
 
     @Override
     public void calcNormalisation (DirectedNetwork net, int[] removed) {
-        int total=0;
-        occurrences_ = new int[net.noNodes_+1];
-        justChosen_=removed;
-        if (justChosen_.length==0) {
+        if (removed.length==0) {
             normalisationConstant_=net.noNodes_;
             random_=true;
         }
         else {
-            int node = justChosen_[justChosen_.length - 1];
+            int total=0;
+            occurrences_ = new int[net.noNodes_+1];
+            int node = removed[removed.length - 1];
             for (int n1 : net.outLinks_.get(node)) {
                 for (int n2 : net.outLinks_.get(n1)) {
                     if (n2 == node)
