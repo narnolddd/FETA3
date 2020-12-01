94433d24f8fb96717c39bcef3a6b6223f31c5233
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2020-12-01 01:01:10 +0000

    Fixed large bug in triangle closure model

--- feta/objectmodels/TriangleClosure.java
+++ feta/objectmodels/TriangleClosure.java
@@ -15,6 +15,8 @@ public class TriangleClosure extends ObjectModelComponent {
     private boolean random_;
 
     public void calcNormalisation (UndirectedNetwork net, int[] removed) {
+        // make an array logging the number of triangles an edge could could close if each node is selected.
+        // This model assumes all links are coming from a newly added node
         occurrences_ = new int[net.noNodes_+2];
         random_=false;
         if (removed.length==0) {
@@ -24,6 +26,7 @@ public class TriangleClosure extends ObjectModelComponent {
         else {
             int total=0;
 
+            // Counts the number of neighbourhoods each node is a member of
             for (int node: removed) {
                 for (int n1 : net.neighbours_.get(node)) {
                     if (n1 == node)
@@ -49,13 +52,15 @@ public class TriangleClosure extends ObjectModelComponent {
     }
 
     public void calcNormalisation (DirectedNetwork net, int[] removed) {
+        occurrences_ = new int[net.noNodes_+2];
+        random_=false;
         if (removed.length==0) {
             normalisationConstant_=net.noNodes_;
             random_=true;
         }
         else {
             int total=0;
-            occurrences_ = new int[net.noNodes_+1];
+            occurrences_ = new int[net.noNodes_+2];
             for (int node: removed) {
                 for (int n1 : net.outLinks_.get(node)) {
                     if (n1 == node)
@@ -65,6 +70,11 @@ public class TriangleClosure extends ObjectModelComponent {
                     occurrences_[n1]++;
                     total++;
                 }
+            }
+
+            for (int n: removed) {
+                total-= occurrences_[n];
+                occurrences_[n]=0;
             }
 
             if (total == 0) {
@@ -79,7 +89,7 @@ public class TriangleClosure extends ObjectModelComponent {
 
     public double calcProbability (UndirectedNetwork net, int node) {
         if (tempConstant_==0 || node >= occurrences_.length) {
-            System.out.println("Yo");
+            // System.out.println("Yo");
             return 0.0;
         }
         if (random_) {
@@ -101,34 +111,43 @@ public class TriangleClosure extends ObjectModelComponent {
     }
 
     public void updateNormalisation(UndirectedNetwork net, int [] removed) {
+
         random_=false;
         if (removed.length==0) {
-            tempConstant_=normalisationConstant_;
-            return;
-        }
-        int node = removed[removed.length-1];
-        int total = 0;
-        for (int n1 : net.neighbours_.get(node)) {
-            if (n1 == node)
-                continue;
-            occurrences_[n1]++;
-        }
-
-        for (int n: removed) {
-            occurrences_[n]=0;
-        }
-
-        for (int val: occurrences_) {
-            total+=val;
-        }
-
-        if (total==0) {
             random_=true;
-            tempConstant_= net.noNodes_-removed.length;
+            tempConstant_=normalisationConstant_;
+        } else {
+            int node = removed[removed.length - 1];
+
+            for (int n1 : net.neighbours_.get(node)) {
+                if (n1 == node)
+                    continue;
+                occurrences_[n1]++;
+            }
+
+            for (int n : removed) {
+                occurrences_[n] = 0;
+            }
+
+            int total = 0;
+            for (int val : occurrences_) {
+                total += val;
+            }
+
+            if (total == 0) {
+                random_ = true;
+                tempConstant_ = net.noNodes_ - removed.length;
+            } else {
+                tempConstant_ = total;
+            }
         }
-        else {
-            tempConstant_= total;
-        }
+//        double const1 = tempConstant_;
+//        calcNormalisation(net, removed);
+//        double const2 = tempConstant_;
+//
+//        if (const1!=const2) {
+//            System.out.println("Without speedup: "+const1+", with speedup "+const2);
+//        }
     }
 
     @Override
