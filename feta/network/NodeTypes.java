09643c30f2889238785cb7c2362879094bb0db9a
Richard G. Clegg <richard@richardclegg.org>
2022-06-16 13:15:22 +0100

    Add convenience function to get nodes of a given type

--- feta/network/NodeTypes.java
+++ feta/network/NodeTypes.java
@@ -9,6 +9,7 @@ public class NodeTypes {
 	private ArrayList <Integer> nodeTypes_;
 	private ArrayList <String> typeNames_;
 	private HashMap <String, Integer> typeNumbers_;
+	private HashMap <Integer, ArrayList<Integer>> nodesByType_;
 	
 	/** set the type of a given node to be a string */
 	public static void setNodeType(int nodeNo, String nodeType) 
@@ -17,8 +18,10 @@ public class NodeTypes {
 		/** If this is a new type add it */
 		if (typeNo == null) {
 			typeNo= addNewType(nodeType);
+			nt_.nodesByType_.put(typeNo, new ArrayList<Integer>());
 		}
 		nt_.nodeTypes_.set(nodeNo,typeNo);
+		nt_.nodesByType_.get(typeNo).add(nodeNo);
 	} 
 	
 	/** Add a new type of node called typeName and return the
@@ -26,6 +29,12 @@ public class NodeTypes {
 	public static Integer addNewType (String typeName) {
 		nt_.typeNames_.add(typeName);
 		return nt_.typeNames_.size()-1;
+	}
+	
+	public static ArrayList<Integer> getNodesOfType (String type)
+	/** Return numbers of all nodes with a given type*/
+	{
+		return nt_.nodesByType_.get(nt_.typeNumbers_.get(type));
 	}
 	
 	/**Get the number associated with the type of a given node*/
