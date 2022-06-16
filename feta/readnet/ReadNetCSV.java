76fd854017e77eb9174386aa8dbe0de095c0f370
Richard G. Clegg <richard@richardclegg.org>
2022-06-16 16:41:54 +0100

    Added base for reading typed links in the CSV reader

--- feta/readnet/ReadNetCSV.java
+++ feta/readnet/ReadNetCSV.java
@@ -8,14 +8,16 @@ public class ReadNetCSV extends ReadNet {
     private final int sourceColumn_;
     private final int dstColumn_;
     private final int timeColumn_;
-    private int sourceTypeColumn_;
-    private int dstTypeColumn_;
+    private final int sourceTypeColumn_;
+    private final int dstTypeColumn_;
 
     public ReadNetCSV(FetaOptions options) {
         super(options);
         sourceColumn_=options.getSourceColumn();
         dstColumn_=options.getDstColumn();
         timeColumn_=options.getTimeColumn();
+        sourceTypeColumn_= options.getSourceTypeColumn();
+        dstTypeColumn_= options.getDstTypeColumn();
     }
 
     @Override
@@ -23,12 +25,18 @@ public class ReadNetCSV extends ReadNet {
         String[] parts = line.split(sep_);
         String node1 = parts[sourceColumn_];
         String node2 = parts[dstColumn_];
+
         long time;
         if (timeColumn_>=0) {
             time = Long.parseLong(parts[timeColumn_]);
         } else {
             time = linkno;
         }
-        return lb_.build(node1, node2, time);
+        if (sourceTypeColumn_ < 0) {
+			return lb_.build(node1, node2, time);
+		}
+        String node1type = parts[sourceTypeColumn_];
+        String node2type = parts[dstTypeColumn_];
+        return lb_.build(node1, node2, time,  node1type, node2type); 
     }
 }
